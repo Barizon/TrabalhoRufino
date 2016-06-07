d5ee157fe8b1f268a97ffbb35cc94ebed068c286
Barizon <albertobarizon@gmail.com>
2016-06-07 20:47:47 -0300

    mudança no formulario de venda

--- src/main/java/br/com/converter/GenericConverter.java
+++ src/main/java/br/com/converter/GenericConverter.java
@@ -10,18 +10,21 @@ import javax.faces.convert.Converter;
  * @author barizon
  */
 public class GenericConverter implements Converter {
-    
+
     private final AbstractService abstractService;
-    
+
     public GenericConverter(AbstractService abstractService) {
         this.abstractService = abstractService;
     }
-    
+
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        if (value.equals("null")) {
+            return null;
+        }
         return abstractService.pesquisar(Long.valueOf(value));
     }
-    
+
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
         return value.toString();
